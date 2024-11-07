074450d8b529f3bc3046ee82e06568a87fd5fa21
baksihwan <baksh999@naver.com>
2024-11-07 20:17:21 +0900

    쓸데ì파일정리==

--- schedule.sql
+++ schedule.sql
@@ -9,12 +9,12 @@ CREATE TABLE SCHEDULE
     password String,
     todo String
 );
-Insert into SCHEDULE(id, title, content, user, createdAt, updatedAt, password, todo)
-values(1,'3주차 강의 복습','3주차 강의내용을 보충하자', 'HWAN',
+INSERT INTO SCHEDULE(id, title, content, user, createdAt, updatedAt, password, todo)
+VALUES(1,'3주차 강의 복습','3주차 강의내용을 보충하자', 'HWAN',
        '2024-11-05', '2024-11-05', '1234', '지금 당장 실행!');
 
-select * from SCHEDULE;    --전체 일정 조회
-select * from SCHEDULE where id=1;   --선택 일정 조회
-update schedule set password= '2930' where id = 1 ;  --선택 일정 수정
-delete from schedule where id =1 ;  --선택 일정 삭제
+SELECT * FROM SCHEDULE;    --전체 일정 조회
+SELECT * FROM SCHEDULE WHERE id= 1;   --선택 일정 조회
+UPDATE schedule SET password= '2930' WHERE id = 1 ;  --선택 일정 수정
+DELETE FROM schedule WHERE id = 1 ;  --선택 일정 삭제
 
